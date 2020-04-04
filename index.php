8ea43aa46412b713307576cb669275725544251c
mantis <war3mantis@gmail.com>
2020-04-04 18:37:33 +0300

    1

--- index.php
+++ index.php
@@ -1,1 +1,116 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+    <title>Title Goes Here</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+
+      </head>
+        <body>
+          <nav>
+            <div class="burger">
+              <div class="line1"></div>
+              <div class="line2"></div>
+              <div class="line3"></div>
+              </div>
+            <div class="logo">
+              <a href="index.html"><img src="images/logo5.jpg"></a>
+            </div>
+            <div class="title">
+              <h1>Title</h1>
+            </div>
+            <div class="phone">
+              <p>0671606967</p>
+            </div>
+              <ul class="nav-links">
+                  <li><a href="Shop.html">Shop</a> </li>
+                  <li><a href="Partnership.html">Partner</a> </li>
+                  <li><a href="Contacts.html">Contacts</a> </li>
+              </ul>
+          </nav>
+          </body>
+          <script src="app.js"></script>
+<!--Header!-->
+<main>
+<a href="construction.html">
+  <picture>
+    <source media="(min-width: 1050px)"
+            srcset= "images/MustardLarge1602.jpg 1602w">
+    <source media="(min-width:700px)"
+            srcset= "images/MustardMedium1422.jpg 1422w,
+            images/MustardSmall819.jpg 819w">
+    <source srcset= "images/MustardSmall819.jpg 819w">
+       <img src="images/MustardSmall819.jpg" alt="Mustard microgreens">
+</picture>
+</a>
+</section>
+<section class="sec1-Text-pos">
+  <a href="construction.html"><p class="sec1-text">Mustard microgreens</p></a>
+</section>
+<section class="sec1-discover-pos">
+  <a href="construction.html"> <p class="sec1-dis">Discover Now</p></a>
+</section>
+<!--section2!-->
+<a href="construction.html">
+<picture>
+  <source media="(min-width: 1050px)"
+          srcset= "images/AmaranthLarge1602.jpg 1602w">
+  <source media="(min-width:700px)"
+          srcset= "images/AmaranthMedium1422.jpg 1422w,
+          images/AmaranthSmall819.jpg 819w">
+  <source
+          srcset= "images/AmaranthSmall819.jpg 819w">
+    <img src="images/AmaranthSmall819.jpg" alt="Amaranth microgreens">
+</picture>
+</a>
+
+  <!--section3!-->
+  <a href="construction.html">
+  <picture>
+    <source media="(min-width: 1050px)"
+            srcset= "images/Amaranth-2Large1602.jpg 1602w">
+    <source media="(min-width:700px)"
+            srcset= "images/Amaranth-2Medium1422.jpg 1422w,
+            images/Amaranth-2Small819.jpg 819w">
+    <source
+            srcset= "images/Amaranth-2Small819.jpg 819w">
+      <img src="images/Amaranth-2Small819.jpg" alt="Amaranth microgreens">
+  </picture>
+</a>
+<div class="sec3-divbox">
+    <p class="sec3-text">Amaranth</p>
+      <a href="construction.html"><p class="sec3-dis">DISCOVER MORE</p></a>
+</div>
+<!--section4!-->
+<a href="construction.html">
+<picture>
+  <source media="(min-width: 1050px)"
+          srcset= "images/KohlrabiLarge1920.jpg 1920w">
+  <source media="(min-width:700px)"
+          srcset= "images/KohlrabiMedium1422.jpg 1422w,
+          images/KohlrabiSmall819.jpg 819w">
+  <source
+          srcset= "images/KohlrabiSmall819.jpg 819w">
+    <img src="images/KohlrabiSmall819.jpg" alt="Kohlrabi microgreens">
+</picture>
+</a>
+  <p class="kohlrabi-text">Kohlrabi </p>
+  <a href="construction.html"><p class="findout">FIND OUT MORE</p> </a>
+<!--section5!-->
+<a href="construction.html">
+<picture>
+  <source media="(min-width: 1050px)"
+          srcset= "images/howtogrowLarge1920.jpg 1920w">
+  <source media="(min-width:700px)"
+          srcset= "images/HowtogrowMedium1422.jpg 1422w,
+          images/HowtoGrowSmall819.jpg 819w">
+  <source
+          srcset= "images/HowtoGrowSmall819.jpg 819w">
+    <img src="images/HowtoGrowSmall819.jpg" alt="HowtoGrow microgreens">
+</picture>
+</a>
+<footer>
+</footer>
+          </body>
+          </html>
